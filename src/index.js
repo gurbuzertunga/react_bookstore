fad63cfc5dd1954e2fca1fb141b1bcb80ae658fd
gurbuzertunga <gurbuzertunga@gmail.com>
2021-03-05 17:28:13 +0300

    initialize store with books

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from "react-redux";
 import './index.css';
 import App from './App';
 
+const state =  { books: [{id:Math.Random(), title:'The Dark Tower', category: 'Horror'}, {id:Math.Random(), title: 'Fantastic Beast', category: 'Sci-Fi'}] };
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root'),
 );
