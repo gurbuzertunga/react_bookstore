67352e7da782b45628417ce0846673e113d06ca0
Bello S Babakolo <fleetbeekay@gmail.com>
2021-03-05 14:39:52 +0100

    Implement the bookslist component ch.10 - 13

---
+++ src/containers/BooksList.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { connect } from "react-redux";
+
+const BooksList = () => {
+  return (
+    <table>
+        <th>
+            <td>Book ID</td>
+            <td>Title</td>
+            <td>Category</td>
+        </th>
+    </table>
+  );
+};
+
+const mapStateToProps = state => {
+    const books = { state };
+    return { books };
+};
+
+export default connect(mapStateToProps)(BookList);
