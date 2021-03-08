b31b89c21a9e00380deed9886981a18bfb4b15eb
gurbuzertunga <gurbuzertunga@gmail.com>
2021-03-08 15:10:44 +0300

    start working on milestone3 req

--- src/containers/BooksForm.js
+++ src/containers/BooksForm.js
@@ -1,18 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+const data = {
+  title: '',
+  category: 'Action',
+};
 
 export default function BooksForm() {
+  const [state, setState] = useState(data);
   const categories = ['Action', 'Biography', 'History', 'Horror', 'Kids', 'Learning', 'Sci-Fi'];
 
+  const handleInputChange = e => {
+    const addBook = { ...state };
+    addBook[e.target.name] = e.target.value;
+    setState(addBook);
+  };
+
+  const handleSubmit = e => {
+    console.log(state);
+    e.preventDefault();
+  };
+
   return (
-    <form onSubmit={() => null}>
+    <form onSubmit={handleSubmit}>
       <label htmlFor="title">
         Title:
-        <input type="text" name="title" id="title" required />
+        <input type="text" name="title" id="title" value={state.title} onChange={handleInputChange} required />
       </label>
 
       <label htmlFor="category">
         Choose a category:
-        <select name="category" id="category">
+        <select name="category" id="category" value={state.category} onChange={handleInputChange}>
           {categories.map(cat => (
             <option key={cat} value={cat}>
               {cat}
