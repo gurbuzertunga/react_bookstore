2a678026daa522ac86b36a68b39a8dfbae3b3931
Bello S Babakolo <fleetbeekay@gmail.com>
2021-03-09 14:53:16 +0100

    Configure tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/* eslint-disable */
+module.exports = {
+  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
